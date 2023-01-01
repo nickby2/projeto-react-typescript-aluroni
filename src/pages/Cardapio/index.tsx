aa25ba90523e1158b8e851a01844bc51de80291d
Lucas <monteiro9961@gmail.com>
2023-01-01 03:58:43 -0300

    Criando o projeto

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,11 @@
+import style from './Cardapio.module.scss'
+
+export default function Cardapio() {
+    return (
+        <main>
+            <nav className="menu">
+
+            </nav>
+        </main>
+    )
+}
